65118b30a3639ab56fbf163a0512ab07d51f24e1
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-08-27 18:50:29 +0530

    case_study_1

--- dannys_diner/solutions.sql
+++ dannys_diner/solutions.sql
@@ -17,3 +17,18 @@ FROM dannys_diner.sales
 GROUP BY customer_id;
 
 -- 3. What was the first item from the menu purchased by each customer?
+
+WITH first_order as(
+SELECT
+	s.customer_id,
+    m.product_name,
+  	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) as order_num
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu m ON s.product_id = m.product_id)
+
+SELECT customer_id, product_name FROM first_order
+WHERE order_num = 1
+GROUP BY customer_id, product_name;
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
