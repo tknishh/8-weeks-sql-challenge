cfad04872279e2634e2045d752bfa73a68968d16
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-08-27 19:14:37 +0530

    case_study_1

--- dannys_diner/solutions.sql
+++ dannys_diner/solutions.sql
@@ -41,3 +41,50 @@ LIMIT 1;
 
 -- 5. Which item was the most popular for each customer?
 
+WITH fav_product as(
+SELECT 
+	s.customer_id, 
+    m.product_name, 
+    COUNT(s.product_id) as buy_freq,
+    DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(s.product_id) DESC) as popularity_level
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu m ON s.product_id = m.product_id
+GROUP BY customer_id, product_name)
+
+SELECT customer_id, product_name, buy_freq
+FROM fav_product
+WHERE popularity_level = 1;
+
+-- 6. Which item was purchased first by the customer after they became a member?
+
+WITH first_order as(
+SELECT
+	s.customer_id,
+    m.product_name,
+  	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) as order_num
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu m ON s.product_id = m.product_id
+JOIN dannys_diner.members me ON me.customer_id = s.customer_id 
+  AND s.order_date > me.join_date)
+
+SELECT customer_id, product_name FROM first_order
+WHERE order_num = 1
+GROUP BY customer_id, product_name;
+
+-- 7. Which item was purchased just before the customer became a member?
+
+WITH first_order as(
+SELECT
+	s.customer_id,
+    m.product_name,
+  	ROW_NUMBER() OVER(PARTITION BY s.customer_id ORDER BY s.order_date DESC) as order_num
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu m ON s.product_id = m.product_id
+JOIN dannys_diner.members me ON me.customer_id = s.customer_id 
+  AND s.order_date < me.join_date)
+
+SELECT customer_id, product_name FROM first_order
+WHERE order_num = 1
+GROUP BY customer_id, product_name;
+
+-- 8. What is the total items and amount spent for each member before they became a member?
